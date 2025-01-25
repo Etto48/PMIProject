b2b7e6d69706a0d54094f0677bb873b951488783
Etto48 <ettore.ricci.coding@gmail.com>
2025-01-25 16:38:21 +0100

    added complaints about docx and fixed tables

--- docs/fix_docx.py
+++ docs/fix_docx.py
@@ -2,12 +2,18 @@ import docx
 import os
 import subprocess as sp
 
+import docx.styles
+import docx.styles.styles
+
 def fix_table_style(docx_file):
     doc = docx.Document(docx_file)
+    # for style in doc.styles:
+    #     print(style.name)
     for table in doc.tables:
-        for row in table.rows:
-            for cell in row.cells:
-                cell.paragraphs[0].style = None
+        table.autofit = True
+        # for row in table.rows:
+        #     for cell in row.cells:
+        #         cell.paragraphs[0].style = None
     doc.save(docx_file)
 
 if __name__ == '__main__':
--- docs/gen_docx.sh
+++ docs/gen_docx.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# For the love of god, I'd like to know why we have to convert
+# from tex to docx. Why tex is not enough? What's better in docx?
+# To this day, I still don't know. But I have to do it.
+
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 cd "$SCRIPT_DIR"
@@ -9,3 +13,4 @@ DOCX_PATH=main.docx
 TEMPLATE_PATH=template.docx
 
 pandoc "$MAIN_PATH" -o "$DOCX_PATH" --reference-doc="$TEMPLATE_PATH"
+python3 fix_docx.py
